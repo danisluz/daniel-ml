adcef384b568cc94e19cf3def1625da1f1e5f2d6
Daniel Luz <29287031+danisluz@users.noreply.github.com>
2023-08-17 15:07:11 -0300

    add curriculum

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   i18n: {
     locales: ["fr", "en", "pt"],
     defaultLocale: "fr",
